5e4de42ce51eb36811f52fcae9f2cd9d627385d9
Joshua Lauterbach <jl@beebucket.de>
2025-03-13 09:31:10 +0100

    Simplify static indices test
    The structure of the test for static indices was quite complicated. Simplify that.

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from enum import Enum
-from logging import getLogger
 from pathlib import PurePosixPath
 from types import NoneType
 from typing import Literal, Union, get_args, get_origin
@@ -9,49 +8,51 @@ from uuid import UUID
 from pydantic import AnyUrl, BaseModel
 
 from extended_dataset_profile import SchemaVersion, schema_versions
+from extended_dataset_profile.models.v0.edp import Numeric
 from extended_dataset_profile.models.v0.languages import Language
-
-logger = getLogger(__name__)
 
 
 # This tests, whether the EDP is always index able with a finite amount of indices.
 def test_finite_edp_index():
-    models_already_processed: set[type[BaseModel]] = set()
-    models_to_process: list[type[BaseModel]] = [schema_versions[SchemaVersion.V0]]
-
-    while len(models_to_process) != 0:
-        model = models_to_process.pop(0)
-        if model not in models_already_processed:
-            models_to_process.extend(_validate_model(model))
-            models_already_processed.add(model)
+    _assert_has_static_indices_only(schema_versions[SchemaVersion.V0])
 
 
-def _validate_model(model_type: type[BaseModel]):
-    logger.info("Checking model %s", model_type.__name__)
-    for field_name, field in model_type.__pydantic_fields__.items():
-        field_type = field.annotation
-        if field_type:
-            try:
-                yield from _validate_model_type(field_type)
-            except TypeError as e:
-                raise TypeError(f"{model_type.__name__}.{field_name}: Type {e} is not supported in the schema!")
-
-
-# Check if the given type is valid for the EDPS model (it must not allow custom indices for fields) and return
-# additional types that need checking.
-def _validate_model_type(current_type: type):
+def _assert_has_static_indices_only(current_type: type):
+    """Check if the given type is valid for the EDPS model (it must not allow custom indices for fields)."""
     if current_type in [NoneType, str, int, float, complex, bool, datetime, timedelta, PurePosixPath, AnyUrl, UUID]:
-        pass
+        return
     elif get_origin(current_type) == Literal:
-        pass
+        return
     elif isinstance(current_type, type) and issubclass(current_type, Enum):
-        pass
+        return
     elif isinstance(current_type, type) and issubclass(current_type, BaseModel):
-        yield current_type
+        for field_name, field in current_type.model_fields.items():
+            field_type = field.annotation
+            if field_type is None:
+                raise TypeError(
+                    f'The field "{field_name}" inside the pydantic model "{current_type}" is missing a type annotation!'
+                )
+            _assert_has_static_indices_only(field_type)
     elif current_type is Language:
-        pass
-    elif get_origin(current_type) in [Union, list, set]:
+        return
+    elif get_origin(current_type) in [list, set]:
         for generic_type in get_args(current_type):
-            yield from _validate_model_type(generic_type)
+            _assert_has_static_indices_only(generic_type)
+    elif get_origin(current_type) is Union:
+        # Special case for optional, which is allowed. get_origin(Optional) will always result in Union.
+        args = get_args(current_type)
+        if len(args) == 2:
+            if None in args:
+                return
+            if NoneType in args:
+                return
+
+        # Special case for numeric, which is the only Union we currently allow.
+        if current_type is Numeric:
+            return
+
+        raise TypeError(
+            f'Found forbidden type union: "{current_type}". These would lead to the same JSON keys containing different types of objects.'
+        )
     else:
-        raise TypeError(str(current_type))
+        raise NotImplementedError(f'The check for "{current_type}" is not yet implemented.')
